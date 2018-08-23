9a1b3bd16ed8f7e922e7b7c1357412909e19063c
Rodel Aragon <zephyr9990@gmail.com>
2018-08-23 00:48:09 -0700

    Declared function noexcept to correct compiler warning

--- HashedDictionary/NotFoundException.cpp
+++ HashedDictionary/NotFoundException.cpp
@@ -18,7 +18,7 @@
 #include "stdafx.h"
 #include "NotFoundException.h"
 
-NotFoundException::NotFoundException(const std::string & message)
+NotFoundException::NotFoundException(const std::string & message) noexcept
 	: logic_error("Precondition violated exception: " + message)
 {
 }// end constructor
